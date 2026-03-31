857b3039b20d0b061fdeccf901ee4a9081350431
GregoryCMSV <121581844+GregoryCMSV@users.noreply.github.com>
2026-03-30 22:27:40 -0300

    Criado Projeto

---
+++ GeradorNotaDeDebito/GeradorNotaDeDebito/FrmGeradorNota.cs
@@ -0,0 +1,15 @@
+namespace GeradorNotaDeDebito
+{
+    public partial class FrmGeradorNota : Form
+    {
+        public FrmGeradorNota()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGeradorNota_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
